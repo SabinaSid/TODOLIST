2154afa86f4f7b2e78677507c5abcfcfa94b3d5f
Sabinca <kaiser2169@gmail.com>
2020-02-14 09:57:12 +0300

    подключен iquery

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express=require('express');
-const urlencoded=require('body-parser').urlencoded({extended:true});
+const bodyParser=require('body-parser');
+const routes=require('./routes');
 
-let port=80;
+//создание приложения
 let app=express();
-
+//установка порта для приложения
+let port=80;
+//задаем шаблонизатор для представления
 app.set('view engine','pug');
-
+//задаем директорию со статическими файлами
 app.use(express.static(__dirname+'public'));
-app.use(urlencoded);
+//задаем парсер тела запроса request body
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/',routes);
+//запуск приложения с прослушивания порта
 app.listen(port,()=>{
     console.info(`App run on http://localhost:${port}`);
 });
