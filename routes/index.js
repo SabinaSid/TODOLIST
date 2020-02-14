69d2f91950e0febe45f9cefdf7c20379d4c7aa38
Sabinca <kaiser2169@gmail.com>
2020-02-14 14:34:05 +0300

    crud операции (работает только add)

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router=require('express').Router();
 const main=require('./main');
+const crud=require('./crud');
 
 //связываем маршрут и обработчик
 router.get('/',main);
+router.get('/get/:id',crud.get);
+router.post('/add',crud.add);
+router.post('/update',crud.update);
+router.get('/delete/:id',crud.delete);
 
 //экспортируем маршрутизатор
 module.exports=router;
