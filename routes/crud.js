02068472f69721518a02957965324f2bedf3d88b
Sabinca <kaiser2169@gmail.com>
2020-02-21 09:31:42 +0300

    crud

--- routes/crud.js
+++ routes/crud.js
@@ -1,30 +1,42 @@
 const {MainViewModel} = require('../models');
-let db=require('../utils/localeStorage');
+let db=require('../utils/sqlitedb');
 
 exports.get=(r,q)=>{
-    var item=db.getTasks(+r.params.id);
-    if(item){
-        let model= new MainViewModel('TODO LIST'
-                                    ,db.getTasks()
-                                    ,db.getStatuses()
-                                    ,item);
-        return q.render('index',model);
-    }
-    q.redirect('/');
+    db.getTasks(+r.params.id).then(item=>{
+        if(item){
+            console.log(item);
+            db.getTasks().then(tasks =>{
+                db.getStatuses().then(statuses =>{
+                    let model= new MainViewModel('TODO LIST',tasks,statuses,item);
+                    q.render('index',model);
+                });
+            });
+        } else{
+            q.redirect('/');
+        }
+    });
 }
 
 exports.add=(r,q)=>{
-    r.body.status=db.getStatuses(+r.body.status);
-    db.addTask(r.body);
-    q.redirect('/');
+    db.getStatuses(+r.body.status).then(status=>{
+        r.body.status=status;
+        db.addTask(r.body).then(x=>{
+            q.redirect('/');
+        });
+    });
 }
+
 exports.update=(r,q)=>{
     r.body.id=+r.body.id;
-    r.body.status=db.getStatuses(+r.body.status);
-    db.updateTask(r.body);
-    q.redirect('/');
+    db.getStatuses(+r.body.status).then(x=>{
+        r.body.status=x;
+        db.updateTask(r.body).then(y=>{
+            q.redirect('/');
+        });
+    });
 }
 exports.delete=(r,q)=>{
-    db.removeTask(+r.params.id);
-    q.redirect('/');
+    db.removeTask(+r.params.id).then(x=>{
+        q.redirect('/');
+    });
 }
--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const db=require('../utils/sqlitedb');
 module.exports=(r,q)=>{
     db.getTasks().then(task=>{
         db.getStatuses().then(statuses=>{
-            let model= new MainViewModel('TODO LIST',db.getTasks(),db.getStatuses());
+            let model= new MainViewModel('TODO LIST',task,statuses);
             q.render('index',model);
         });
     }); 
